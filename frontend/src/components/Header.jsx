f3396155c97689441ca6e5dcf48b690b865f646b
NN90 <nhat.nm90a@gmail.com>
2025-11-17 09:55:10 +0700

    Update title

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     // space-y-2: để những phần tử con cách nhau ra
     <div className='space-y-2 text-center'>
       {/* text-transparent */}
-      <h1 className='text-4xl font-bold text-transparent bg-primary bg-clip-text'>TodoX</h1>
+      <h1 className='text-4xl font-bold text-transparent bg-primary bg-clip-text'>N Todo</h1>
       <p className='text-muted-foreground'>Care - Control - Concentrate</p>
     </div>
   )
